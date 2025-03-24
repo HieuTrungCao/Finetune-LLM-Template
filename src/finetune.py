715f7ba9b667568caaf2eab1066ee6ed88de6382
HieuTrungCao <21021490@vnu.edu.vn>
2025-03-24 11:42:23 +0700

    change config to fix type

--- src/finetune.py
+++ src/finetune.py
@@ -19,8 +19,6 @@ def train(config):
             print("Wandb init!")
             wandb.init(project=config["log"]["project_name"])
     
-    config = convert_list_config_to_list(config)
-
     model, tokenizer, peft_config = LLM.load_model(config)
     
     peft_config = convert_list_config_to_list(peft_config)
@@ -28,9 +26,9 @@ def train(config):
     llm_dataset = LLM_Dataset(config, tokenizer)
     finetune_dataset, valid_dataset = llm_dataset.get_dataset()
 
-    training_arguments = hydra.utils.instantiate(config["training_arg"])
+    training_arguments = hydra.utils.instantiate(config.training_arg)
     
-    trainer = hydra.utils.instantiate(config["trainer"],
+    trainer = hydra.utils.instantiate(config.trainer,
         model=model,
         train_dataset=finetune_dataset,
         eval_dataset=valid_dataset,
